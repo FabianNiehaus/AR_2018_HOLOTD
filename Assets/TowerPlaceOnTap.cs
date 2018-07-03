52856239d5aa60d6e0727f8cfd4dcabccacec086
Yannik <yannik@trustet.de>
2018-07-03 17:13:15 +0200

    TapToPlace Script dem Cursor hinzugefügt

---
+++ Assets/TowerPlaceOnTap.cs
@@ -0,0 +1,34 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlaceOnTap : MonoBehaviour, IInputClickHandler
+{
+
+    public GameObject original;
+    private float lastClickTime = 0;
+    private float debounceDelay = 0.115f;
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (Time.time - lastClickTime < debounceDelay)
+        {
+            return;
+        }
+
+        lastClickTime = Time.time;
+        GameObject cube = GameObject.Instantiate(original);
+        cube.transform.position = GetComponent<Transform>().position;
+        Debug.Log("AirTap!");
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
